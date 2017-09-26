422d3bfc2bed22df1295a622dc063fb74f1b6e95
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-09-26 12:30:03 +0100

    Implemented Logging Functions

--- logger.js
+++ logger.js
@@ -1,4 +1,93 @@
 'use-strict';
 
-const account_info = require('./auth/client_secret');
-const GoogleSpreadsheet = require('google-spreadsheet';
+const GoogleSpreadsheet = require('google-spreadsheet');
+const credentials = require('./auth/client_secret');
+const config = require('./auth/config');
+const moment = require('moment');
+var document = new GoogleSpreadsheet(config.spreadsheet_id);
+
+module.exports = {
+    storeData: storeData,
+    storeRunningData: storeRunningData
+};
+
+/**
+ * @desc Stores entry location data into spreadsheet
+ * @param {moment} startTime 
+ * @param {moment} endTime 
+ * @param {moment} startDate 
+ * @param {moment} endDate 
+ */
+function storeData(startTime, endTime, startDate, endDate) {
+
+    document.useServiceAccountAuth(credentials, function(err){
+        if(err)
+            console.log(err);
+
+            document.addRow(1, generateData(startTime, endTime, startDate, endDate), function(err) {
+                    if(err)
+                        console.log(err);
+                });
+            });
+}
+
+/**
+ * @desc stores running data into spreadsheet
+ * @param {moment} morningRunTime 
+ * @param {moment} eveningRunTime 
+ * @param {moment} date 
+ */
+function storeRunningData(morningRunTime, eveningRunTime, date) {
+    document.useServiceAccountAuth(credentials, function(err){
+        if(err)
+            console.log(err);
+
+            document.addRow(2, generateRunningData(morningRunTime, eveningRunTime, date), function(err) {
+                    if(err)
+                        console.log(err);
+                });
+            });
+
+}
+
+function generateData(startTime, endTime, startDate, endDate) {
+    return {
+        leavedate: startDate ? startDate.format('DD/MM/YYYY') : 'NA',
+        leavetime: startTime ? startTime.format('hh:mm') : 'NA',
+        enterdate: endDate ? endDate.format('DD/MM/YYYY') : 'NA',
+        entertime: endTime ? endTime.format('hh:mm') : 'NA',
+    };
+}
+
+function generateRunningData(morningRunTime, eveningRunTime, date) {
+    return {
+        morningruntime: morningRunTime ? normalizeDuration(morningRunTime) : 'NA',
+        eveningruntime: eveningRunTime ? normalizeDuration(eveningRunTime) : 'NA',
+        date: date ? date.format('DD/MM/YYYY') : 'NA',
+    }
+}
+
+
+function padleftInt(integral, width){
+    let value = Math.pow(10, width - 1);
+    let i = 0;
+    let result = '';
+    do{
+        if(integral < value) {
+            result += '0';
+            value /= 10;
+        } else {
+            result += integral;
+            break;
+        }
+        i = i + 1;
+    } while(i < width);
+
+    return result;
+
+}
+
+function normalizeDuration(duration) {
+    return (duration.hours() == 0 ? '' : padleftInt(Number(duration.hours()), 2) + ':') + padleftInt(Number(duration.minutes()), 2) + ':' + padleftInt(Number(duration.seconds()), 2);
+}
+
